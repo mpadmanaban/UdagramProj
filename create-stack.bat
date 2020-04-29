949f05646d788b1a4582d00fe02cac6d6e2e7a74
mpadmanaban <mpadmanaban@gmail.com>
2020-04-29 11:10:48 -0700

    Add Latest changes with BastionHost

--- create-stack.bat
+++ create-stack.bat
@@ -1,5 +1,5 @@
 :: Command to Create AWS Stack for High-Availability webapp
-echo off
+@echo off
 setlocal
 
 if "%1" == "-h" goto USAGE
@@ -9,23 +9,15 @@ if "%1" == "update"  goto UPDATE
 goto USAGE
 
 :CREATE_NEW
-create.bat" UdagramNetwork udagram-nw.yml udagram-nw.json
-create.bat UdagramServers udagram-srv.yml udagram-srv.json
+call create.bat UdagramNetwork udagram-nw.yml udagram-nw.json && call create.bat UdagramServers udagram-srv.yml udagram-srv.json
 goto END
 
 :UPDATE
-update.bat" UdagramNetwork udagram-nw.yml udagram-nw.json
-update.bat UdagramServers udagram-srv.yml udagram-srv.json
+call update.bat UdagramNetwork udagram-nw.yml udagram-nw.json
+call update.bat UdagramServers udagram-srv.yml udagram-srv.json
 goto END
 
 :USAGE 
- echo Please run create-stack.bat options
- echo Command to Create AWS Stack for High-Availability webapp
- echo  options: new|update|-h|/?
- echo     new   : create new stack
- echo     update: create new stack
- echo     -h   : create new stack
- echo     /?   : create new stack
  
 
 :END
