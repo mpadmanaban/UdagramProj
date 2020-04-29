179c9b7b6e3d7b4015cf422e2b14dbd595af794b
mpadmanaban <mpadmanaban@gmail.com>
2020-04-29 11:11:06 -0700

    Update echo statements

--- create-stack.bat
+++ create-stack.bat
@@ -18,6 +18,13 @@ call update.bat UdagramServers udagram-srv.yml udagram-srv.json
 goto END
 
 :USAGE 
+@echo "Please run create-stack.bat <option>"
+@echo "Command to Create AWS Stack for High-Availability webapp"
+@echo "  valid options are: new|update|-h|/?"
+@echo "     new   : create new stack"
+@echo "     update: create new stack"
+@echo "    -h     : create new stack"
+@echo "    /?     : create new stack"
  
 
 :END
